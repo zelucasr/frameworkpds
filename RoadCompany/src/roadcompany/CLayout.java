0a0957bd87028077d01efe530024d05155b57ede
zelucasr <zelucasr@gmail.com>
2015-05-30 23:45:47 -0300

    adicionando nova forma de manipular a interface

--- RoadCompany/src/roadcompany/CLayout.java
+++ RoadCompany/src/roadcompany/CLayout.java
@@ -12,36 +12,42 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class CLayout {
-        JFrame frame = new JFrame("CardLayout do sistema");
+        JFrame frame = new JFrame("RoadCompany");
         JPanel panelCont = new JPanel();
-        JLabel jLabel2 = new JLabel();
         JPanel TelaGeral = new JPanel();
-        JButton btCVeiculo = new JButton();
-        JLabel jLabel1 = new JLabel();
+        JButton btCVeiculo = new JButton("Ir para a outra tela");
+        JLabel busao = new JLabel();
         JPanel CadastroVeiculo = new JPanel();
+        CardLayout c1 = new CardLayout();
         
         
         
         public CLayout(){
             initComponents();
-            CardLayout c1 = new CardLayout();
+            initTelaGeral();
+            panelCont.setLayout(c1);
             panelCont.add(TelaGeral,"1");
             panelCont.add(CadastroVeiculo,"2");
-            c1.show(panelCont,"1");
             
             frame.add(panelCont);
+            frame.setResizable(false);
             frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
             frame.pack();
             frame.setVisible(true);
             
+            c1.show(panelCont,"1");
+            
             btCVeiculo.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e) {
+                initCadastroVeiculo();
                 c1.show(panelCont,"2");
+                
             }
-        });
+            });
             
         }
         
@@ -54,16 +60,54 @@ public class CLayout {
         }
         
         
-     private void initComponents() {
-         TelaGeral.setPreferredSize(new java.awt.Dimension(800, 620));
-         CadastroVeiculo.setPreferredSize(new java.awt.Dimension(800, 620));
-         jLabel1.setIcon(new javax.swing.ImageIcon("C:\\Users\\Ad\\Documents\\GitHub\\frameworkpds\\RoadCompany\\src\\roadcompany\\img\\mainwindow.jpg"));
-         TelaGeral.add(jLabel1);
-         CadastroVeiculo.add(jLabel1);
-         jLabel1.setBounds(0, 0, 800, 600);
-         TelaGeral.setBounds(0, 0, 800, 620);
+     private void initComponents(){
+         busao.setIcon(new javax.swing.ImageIcon("C:\\Users\\Zé\\Documents\\GitHub\\projetopds\\frameworkpds\\RoadCompany\\src\\roadcompany\\img\\mainwindow.jpg"));
+     }
+     
+     private void initTelaGeral() {
+         TelaGeral.setLayout(null);
+         TelaGeral.setPreferredSize(new java.awt.Dimension(800, 600));
+         TelaGeral.setSize(800, 600);
          
+         TelaGeral.add(busao);
          
-     }   
-        
+         busao.setBounds(0, 0, 800, 600);
+         btCVeiculo.setBounds(50, 40, 200, 120);
+         
+         TelaGeral.add(btCVeiculo);
+     }
+     
+     private void initCadastroVeiculo(){ //Tela de cadastro de Veículo
+         CadastroVeiculo.setLayout(null);
+         CadastroVeiculo.setPreferredSize(new java.awt.Dimension(800, 600));
+         CadastroVeiculo.setSize(800, 600);
+         
+            /*-Modelo
+            -Ano
+            -Placa
+            -Disponibilidade (bool)
+            -DataRevisão
+            -Observações*/
+         JTextField tModelo = new JTextField();
+         JTextField tPlaca = new JTextField();
+         JTextField tDisponivel = new JTextField();
+         JTextField tRev = new JTextField();
+         JTextField tObs = new JTextField();
+         CadastroVeiculo.add(tModelo);
+         CadastroVeiculo.add(tPlaca);
+         CadastroVeiculo.add(tDisponivel);
+         CadastroVeiculo.add(tRev);
+         CadastroVeiculo.add(tObs);
+         
+         tModelo.setBounds(50, 40, 150, 25);
+         tPlaca.setBounds(50, 85, 150, 25);
+         tDisponivel.setBounds(50, 130, 150, 25);
+         tRev.setBounds(50, 175, 150, 25);
+         tObs.setBounds(50, 220, 150, 25);
+         CadastroVeiculo.add(busao);
+         busao.setBounds(0, 0, 800, 600);
+     }
+     
+     
+     
 }
